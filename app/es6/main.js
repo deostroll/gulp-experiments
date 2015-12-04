18c850c5e9b68e55da8145883aff8e019c05a348
deostroll <deostroll@gmail.com>
2015-12-04 19:18:09 +0530

    initial commit

---
+++ app/es6/main.js
@@ -0,0 +1,19 @@
+import {sum, pi} from "calc"
+class Greeter {
+  constructor(msg) {
+    this.message = msg;
+  }
+
+  getGreeting() {
+    return this.message;
+  }
+
+  getValue() {
+    return sum(3,4);
+  }
+}
+
+window.addEventListener('load',() => {
+  var cls = new Greeter('Hello World');
+  document.getElementById('content').innerHTML = cls.getGreeting();
+}, false);
---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+var gulp = require('gulp');
+var wiredep = require('wiredep').stream;
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+// var babel = require('babel-core');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
+var fs = require('fs');
+// var BufferStream = require('./lib/buffer-stream');
+var _ = require('lodash');
+
+gulp.task('wiredep', function(){
+  gulp.src('./app/index.html')
+    .pipe(wiredep())
+    .pipe(gulp.dest('./app'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('bower.json', ['wiredep']);
+
+  gulp.watch(['*.html', 'styles/style.css', 'scripts/main.js'], {
+    cwd:'app'
+  }, reload);
+
+  gulp.watch('app/es6/**/*.js', ['browserify']);
+
+});
+
+// gulp.task('babelify', function(){
+//   var src = fs.createWriteStream('app/scripts/main.js');
+//   babel.transformFile('app/es6/main.js', function(err, result){    // console.log(result.code);
+//     if(err) {
+//       console.log(err);
+//       return;
+//     }
+//
+//     var code = new BufferStream(result.code);
+//     code.pipe(src);
+//
+//   });
+//   return src;
+// });
+
+var config = {
+  entryFile: './app/es6/main.js',
+  outputDir: 'app/scripts/',
+  outputFile: 'app/scripts/main.js'
+};
+
+var source = function(file) {
+  return fs.createWriteStream(file);
+};
+
+// var _ = {
+//   extend: function(dest, source) {
+//     for(var key in source) {
+//       dest[key] = source[key];
+//     }
+//     return dest[key]
+//   }
+// };
+
+var bundler = watchify(
+  browserify(config.entryFile)
+);
+
+bundler.on('update', function(){
+  console.log("updated");
+  gulp.start('build');
+})
+gulp.task('build', function(){
+  return bundler
+    .transform(babelify({ loose: 'all' }))
+    .bundle()
+    .on('error', function(err){
+      console.log(err.message);
+    })
+    .pipe(source(config.outputFile));
+    // .pipe(reload({ stream: true}))
+});
+
+gulp.task('serve', function(){
+  return browserSync({
+    open: false,
+    server: {
+      baseDir: 'app'
+    }
+  });
+});
+
+gulp.task('default', ['wiredep', 'build', 'serve', 'watch']);
